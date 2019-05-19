261aeb179b9d4e37744b2dd21d353a4efebaa2f7
David Krystall <dkrystall@gmail.com>
2019-05-19 05:19:03 -0700

    app works with browser gps now

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="jumbotron">
+        <h4 class="display-4">
+            My Location
+        </h4>
+        <p class="lead">Enter your latitude and longitude to display your location on a Google Map</p>
+        <hr>
+        <form @submit.prevent="handleSubmit">
+            <div class="form-row">
+                <div class="col">
+                    <input v-model="geoLat" type="text" name="lat" id="lat" placeholder="Latitude" class="form-control">
+                </div>
+                <div class="col">
+                    <input v-model="geoLong" type="text" name="lon" id="lon" placeholder="Longitude" class="form-control">
+                </div>
+                <div class="col">
+                  <button type="button" @click="getUsersGeo()"><img class="icon-button" src="https://cdn2.iconfinder.com/data/icons/freecns-cumulus/32/519777-89_Compass-512.png"></button>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col">
+                    <button type="submit" class="btn btn-primary mt-4">Show My Location</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <my-map name="map" v-if="!isHidden" :lat="latitude" :lon="longitude" msg="oh no"></my-map>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyMap from './components/my-map.vue'
 
 export default {
+  
+  data: function () {
+    return {
+      isHidden: true,
+      lati:String,
+      long:String,
+      geoLat:"",
+      geoLong:""
+    }
+  },
   name: 'app',
   components: {
-    HelloWorld
+    MyMap
+  },
+  computed: {
+    latitude: function () {
+      return this.lati
+    },
+    longitude: function () {
+      return this.long
+    }
+  },
+  methods: {
+    handleSubmit(submitEvent) {
+      this.isHidden = false
+      this.lati = submitEvent.target.elements.lat.value
+      this.long = submitEvent.target.elements.lon.value
+    },
+    getUsersGeo() {		
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(this.autoEnterCoords);
+      }else{
+        this.error = "Geolocation is not supported."; 
+      }
+    },
+    autoEnterCoords(position) {	
+      this.geoLat= position.coords.latitude;
+      this.geoLong = position.coords.longitude;
+    }
   }
 }
 </script>
@@ -24,5 +84,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.icon-button{
+  height: 35px;
+  width: 35px;
 }
 </style>
---
+++ src/components/my-map.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{lat}},{{lon}}</h1>
+    <img :src="`http://maps.googleapis.com/maps/api/staticmap?center=${lat},${lon}&markers=${lat},${lon}&zoom=14&size=400x400&key=AIzaSyC0MAKJQQaF2SY2iaZjSQULtuCCN3QzLiM`">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-map',
+  props: {
+    lat: String,
+    lon: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
